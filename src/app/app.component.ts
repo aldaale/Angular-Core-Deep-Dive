6094859de6b7c738bdf22f89c963eab43457e062
Aleksandra Dahlqvist <s2000575@edu.bc.fi>
2021-02-08 16:26:03 +0200

    test built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,12 @@ export class AppComponent {
   
   courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
+  title = COURSES[0].description;
+  price = 9.99;
+  rate = 0.55;
+  course = COURSES[0];
+
   // data = {
   //   title: 'angularkakka'
   // };
