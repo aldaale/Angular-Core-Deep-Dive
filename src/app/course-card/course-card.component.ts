8c182b7e45d128e5c056e811a33d25b8554a2830
Aleksandra Dahlqvist <s2000575@edu.bc.fi>
2021-02-08 12:47:24 +0200

    test ngIf

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -28,4 +28,8 @@ export class CourseCardComponent implements OnInit {
     this.courseSelected.emit(this.course);
   }
 
+  isImageVisible(){
+    return this.course && this.course.iconUrl;
+  }
+
 }
